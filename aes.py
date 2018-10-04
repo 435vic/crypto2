7e527f66d7fb503b3231b120c8259090701cdc91
Victor Javier <victorjavier2004@hotmail.com>
2018-10-04 08:57:57 -0500

    add tqdm progress bar

--- aes.py
+++ aes.py
@@ -5,6 +5,7 @@ from Cryptodome.Cipher import AES
 from Cryptodome.Hash.SHA256 import SHA256Hash
 from Cryptodome import Random
 from Cryptodome.Util.Padding import pad
+from tqdm import tqdm
 
 IV_BLOCK_SIZE = 16
 
@@ -19,11 +20,16 @@ def encrypt(key, fileIn, fileOut=None, chunksize=64 * 1024):
     iv = Random.new().read(AES.block_size)
     encryptor = AES.new(key, AES.MODE_CBC, iv)
     filesize = os.path.getsize(fileIn)
+    print(filesize)
+    encfilesize = filesize + (AES.block_size - filesize % AES.block_size) + 24
+    print(encfilesize)
+    pbar = tqdm(desc='Encrypting', total=encfilesize, unit='Bytes', unit_scale=1)
 
     with open(fileIn, 'rb') as infile:
         with open(fileOut, 'wb') as outfile:
             outfile.write(struct.pack('<Q', filesize))
             outfile.write(iv)
+            pbar.update(24)
 
             while True:
                 chunk = infile.read(chunksize)
@@ -33,6 +39,10 @@ def encrypt(key, fileIn, fileOut=None, chunksize=64 * 1024):
                     chunk = pad(chunk, IV_BLOCK_SIZE)
 
                 outfile.write(encryptor.encrypt(chunk))
+                pbar.update(chunksize)
+    
+    pbar.close()
+    _tmp = input('Press enter to continue...')
 
 
 def decrypt(key, fileIn, fileOut=None, chunksize=24 * 1024):
@@ -45,14 +55,21 @@ def decrypt(key, fileIn, fileOut=None, chunksize=24 * 1024):
         iv = infile.read(IV_BLOCK_SIZE)
         decryptor = AES.new(key, AES.MODE_CBC, iv)
 
+        pbar = tqdm(desc='Decrypting', total=origsize - 24, unit='B', unit_scale=1)
+
         with open(fileOut, 'wb') as outfile:
             while True:
                 chunk = infile.read(chunksize)
                 if len(chunk) == 0:
                     break
+
                 outfile.write(decryptor.decrypt(chunk))
+                pbar.update(chunksize)
 
             outfile.truncate(origsize)
+    
+    pbar.close()
+    _tmp = input('Press enter to continue...')
 
 #endregion Functions
 
